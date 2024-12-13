b69c3ad940e1506bb0c471dacff2af64b23dd38a
Jim Stewart <jim.stewart@thelangschool.org>
2024-12-13 08:27:46 -0500

    reset to start

--- Ball.py
+++ Ball.py
@@ -3,16 +3,4 @@ from pygame.sprite import Sprite
 
 
 class Ball(Sprite):
-    def __init__(self, x, y , vx, vy):
-        Sprite.__init__(self)
-        self.image = pygame.image.load("images/ball.png")
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-        self.vx = vx
-        self.vy = vy
-
-    def update(self):
-        self.rect.x += self.vx
-        self.rect.y += self.vy
-
+    pass
